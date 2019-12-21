aa2e1fe39010c4a19e3c6b5346a61bc3fa15dbe5
Ahmed <hello@clivern.com>
2019-12-21 16:20:18 +0100

    Create doctl.sh

---
+++ third_party/doctl.sh
@@ -0,0 +1,7 @@
+cd /tmp
+
+export DOCTL_VERSION=1.36.0
+echo "Installing Doctl v$DOCTL_VERSION"
+
+curl -sL https://github.com/digitalocean/doctl/releases/download/v{$DOCTL_VERSION}/doctl-{$DOCTL_VERSION}-darwin-amd64.tar.gz | tar xz
+mv doctl /usr/local/bin/
