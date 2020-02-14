ed76d0a9a18d7f183564a7d836b0019abe643ef5
Clivern <hello@clivern.com>
2020-02-14 18:16:44 +0100

    bump gh client

--- third_party/github.sh
+++ third_party/github.sh
@@ -1,6 +1,6 @@
 cd /tmp
 
-export GITHUB_VERSION=0.5.5-pre.1
+export GITHUB_VERSION=0.5.5
 echo "Installing Github v$GITHUB_VERSION"
 
 curl -sL https://github.com/cli/cli/releases/download/v{$GITHUB_VERSION}/gh_{$GITHUB_VERSION}_macOS_amd64.tar.gz | tar xz
