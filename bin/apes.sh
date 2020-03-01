0baf63f50280a6f1773fbf9ee87876e667fbab65
Clivern <hello@clivern.com>
2020-03-01 14:02:56 +0100

    bump apes & rhino

--- bin/apes.sh
+++ bin/apes.sh
@@ -1,6 +1,6 @@
 cd /tmp
 
-export APES_VERSION=0.1.0
+export APES_VERSION=0.1.1
 export APES_LATEST_VERSION=$(curl --silent "https://api.github.com/repos/Clivern/Apes/releases/latest" | jq '.tag_name' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "Installing Apes $APES_VERSION. Latest version is $APES_LATEST_VERSION"
 
