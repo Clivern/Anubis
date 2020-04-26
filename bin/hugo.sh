c680899491c983490138395ef16e0cb2f8d52285
Clivern <hello@clivern.com>
2020-04-27 01:18:00 +0200

    hugo extended

--- bin/hugo.sh
+++ bin/hugo.sh
@@ -4,7 +4,7 @@ export HUGO_VERSION=0.69.2
 export HUGO_LATEST_VERSION=$(curl --silent "https://api.github.com/repos/gohugoio/hugo/releases/latest" | jq '.tag_name' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "Installing Hugo v$HUGO_VERSION. Latest version is $HUGO_LATEST_VERSION"
 
-curl -sL https://github.com/gohugoio/hugo/releases/download/v{$HUGO_VERSION}/hugo_{$HUGO_VERSION}_macOS-64bit.tar.gz | tar xz
+curl -sL https://github.com/gohugoio/hugo/releases/download/v{$HUGO_VERSION}/hugo_extended_{$HUGO_VERSION}_macOS-64bit.tar.gz | tar xz
 
 chmod +x ./hugo
 
