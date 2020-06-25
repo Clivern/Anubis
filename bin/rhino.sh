d94a9c6ca68907538e15b254d6c0a1e2077aac13
Clivern <hello@clivern.com>
2020-06-25 20:09:06 +0200

    bump rhino

--- bin/rhino.sh
+++ bin/rhino.sh
@@ -1,6 +1,6 @@
 cd /tmp
 
-export RHINO_VERSION=0.2.0
+export RHINO_VERSION=1.0.0
 export RHINO_LATEST_VERSION=$(curl --silent "https://api.github.com/repos/Clivern/Rhino/releases/latest" | jq '.tag_name' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "Installing Rhino $RHINO_VERSION. Latest version is $RHINO_LATEST_VERSION"
 
