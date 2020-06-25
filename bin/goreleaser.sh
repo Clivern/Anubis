ac8f0c35b954f118ba1117e8524ed946b8f507ca
Clivern <hello@clivern.com>
2020-06-25 22:12:02 +0200

    bump goreleaser

--- bin/goreleaser.sh
+++ bin/goreleaser.sh
@@ -1,6 +1,6 @@
 cd /tmp
 
-export GO_RELEASER_VERSION=0.132.1
+export GO_RELEASER_VERSION=0.138.0
 export GO_RELEASER_LATEST_VERSION=$(curl --silent "https://api.github.com/repos/goreleaser/goreleaser/releases/latest" | jq '.tag_name' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "Installing GoReleaser v$GO_RELEASER_VERSION. Latest version is $GO_RELEASER_LATEST_VERSION"
 
