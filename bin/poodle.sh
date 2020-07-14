c796ce037c196e86588c3dc638693601c2565eb8
Clivern <hello@clivern.com>
2020-07-15 00:20:51 +0200

    update

--- bin/poodle.sh
+++ bin/poodle.sh
@@ -1,11 +1,11 @@
 cd /tmp
 
-export POODLE_VERSION=0.0.2
+export POODLE_VERSION=0.0.3
 export POODLE_LATEST_VERSION=$(curl --silent "https://api.github.com/repos/Clivern/Poodle/releases/latest" | jq '.tag_name' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "Installing Poodle $POODLE_VERSION. Latest version is $POODLE_LATEST_VERSION"
 
-curl -sL https://github.com/Clivern/Poodle/releases/download/{$POODLE_VERSION}/Poodle_{$POODLE_VERSION}_Darwin_x86_64.tar.gz | tar xz
+curl -sL https://github.com/Clivern/Poodle/releases/download/{$POODLE_VERSION}/poodle_{$POODLE_VERSION}_Darwin_x86_64.tar.gz | tar xz
 
-chmod +x ./Poodle
+chmod +x ./poodle
 
-mv -f Poodle /usr/local/bin/poodle
+mv -f poodle /usr/local/bin/poodle
