cc33f028fcf372767432f64c43ff861d020619ca
Clivern <hello@clivern.com>
2020-11-03 20:41:13 +0100

    bump tf

--- bin/terraform.sh
+++ bin/terraform.sh
@@ -1,6 +1,6 @@
 cd /tmp
 
-export TERRAFORM_VERSION=0.13.1
+export TERRAFORM_VERSION=0.13.5
 echo "Installing Terraform v$TERRAFORM_VERSION"
 
 curl -sS https://releases.hashicorp.com/terraform/{$TERRAFORM_VERSION}/terraform_{$TERRAFORM_VERSION}_darwin_amd64.zip > terraform.zip
