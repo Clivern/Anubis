0d7138d5b4725a175be7cd32c8b61bc955a6b4f5
Clivern <hello@clivern.com>
2021-09-03 15:42:35 +0200

    cleanup

--- bin/npm_pkgs.sh
+++ bin/npm_pkgs.sh
@@ -1,14 +1,3 @@
-# Vue cli
 npm install -g @vue/cli
-
-# Angular Cli
-npm install -g @angular/cli@latest
-
+npm install -g @angular/cli
 npm install -g typescript
-
-npm install -g webpack
-
-npm install -g grunt-cli
-
-npm install --global yo
-
