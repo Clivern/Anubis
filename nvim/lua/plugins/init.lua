a6fdb79a687f44399b23cb34c5d457c348088212
Clivern <hello@clivern.com>
2022-10-18 16:03:01 +0200

    chore: adjust syntax highlight

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -30,22 +30,22 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+  			"html", "css", "java",
+				"elixir", "php", "python",
+				"rust", "go", "markdown",
+				"javascript", "json", "yaml"
+  		},
+  	},
+  },
 }
