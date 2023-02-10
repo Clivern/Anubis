bee6b1472fe8aec24d9863b72bd935abb8be5ed6
Clivern <hello@clivern.com>
2023-02-10 22:56:04 +0100

    update

--- homelab/home.ino
+++ homelab/home.ino
@@ -4,14 +4,25 @@
 
 int soundPin = A0;
 int soundVal = 0;
+int soundSum = 0;
+int soundMeasures = 128;
+int soundLevel = 0;
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-  soundVal = analogRead(soundPin);
-  String soundMessage = "{\"type\":\"gauge\",\"name\":\"uno_sound_reading\",\"help\":\"The sound reading.\",\"method\":\"set\",\"value\":" + String(soundVal) + ",\"labels\":{\"country\":\"nl\"}}";
+  soundSum = 0;
+  soundVal = 0;
+  for (int i = 0; i < soundMeasures; i++)  {
+    soundVal = analogRead (soundPin);
+    soundSum = soundSum + soundVal;
+  }
+
+  soundLevel = soundSum / soundMeasures;
+
+  String soundMessage = "{\"type\":\"gauge\",\"name\":\"uno_sound_reading\",\"help\":\"The sound reading.\",\"method\":\"set\",\"value\":" + String(soundLevel) + ",\"labels\":{\"country\":\"nl\"}}";
   Serial.write(soundMessage.c_str());
   Serial.write("\n");
   Serial.write("{\"type\":\"gauge\",\"name\":\"uno_up\",\"help\":\"The arduino status.\",\"method\":\"set\",\"value\":1,\"labels\":{\"country\":\"nl\"}}");
