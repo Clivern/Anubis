a8465d327ba2911e0f8bfb42117b82b221785176
Clivern <hello@clivern.com>
2023-02-10 20:57:14 +0100

    update

--- homelab/home.ino
+++ homelab/home.ino
@@ -2,11 +2,18 @@
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
+int soundPin = A0;
+int soundVal = 0;
+
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
+  soundVal = analogRead(soundPin);
+  String soundMessage = "{\"type\":\"gauge\",\"name\":\"uno_sound_reading\",\"help\":\"The sound reading.\",\"method\":\"set\",\"value\":" + String(soundVal) + ",\"labels\":{\"country\":\"nl\"}}";
+  Serial.write(soundMessage.c_str());
+  Serial.write("\n");
   Serial.write("{\"type\":\"gauge\",\"name\":\"uno_up\",\"help\":\"The arduino status.\",\"method\":\"set\",\"value\":1,\"labels\":{\"country\":\"nl\"}}");
   delay(3000);
 }
