54ad2e696e5199d0d02a7b7dbd1a2b96a1a92e80
Clivern <hello@clivern.com>
2023-07-30 13:43:23 +0200

    support hcl & tf files

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -25,35 +25,48 @@ SOFTWARE.
 --]]
 
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-        "html", "css", "java",
-        "elixir", "php", "python",
-        "rust", "go", "markdown",
-        "javascript", "json", "yaml",
-        "kotlin", "heex", "hcl", "terraform"
-  		},
-  	},
-  },
-  {
-    "clivern/teacup.nvim",
-  },
-  {
-    "clivern/tuk.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform"
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim",
+                "lua",
+                "vimdoc",
+                "html",
+                "css",
+                "java",
+                "elixir",
+                "php",
+                "python",
+                "rust",
+                "go",
+                "markdown",
+                "javascript",
+                "json",
+                "yaml",
+                "kotlin",
+                "heex",
+                "hcl",
+                "terraform"
+            }
+        }
+    },
+    {
+        "clivern/teacup.nvim"
+    },
+    {
+        "clivern/tuk.nvim",
+        dependencies = {"nvim-lua/plenary.nvim"}
+    }
 }
